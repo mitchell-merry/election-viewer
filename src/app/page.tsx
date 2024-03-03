b71dd6c7b79bfaba7d0ccc56f37b973481249588
Mitchell Merry <mmerry@atlassian.com>
2024-03-03 22:46:45 +1100

    Map is loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@
 
 import * as d3 from 'd3';
 import { useEffect, useRef, useState } from 'react';
+// @ts-ignore Not my problem
+import rewind from '@turf/rewind';
 import { type Map } from '@/lib/types';
 import { loadMap } from '@/lib/maps';
+
+const WIDTH = 1000;
+const HEIGHT = 800;
 
 export default function Home() {
   const [error, setError] = useState<Error | undefined>();
@@ -11,21 +16,33 @@ export default function Home() {
   const svgRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
-    loadMap().then(setMapData).catch(setError);
+    loadMap().then(data => {
+      // geoJSON.features = geoJSON.features.map((f) => turf.rewind(f,{reverse:true}));
+      const rewindedData: Map = {
+        ...data,
+        features: data.features.map(f => rewind(f, { reverse: true }))
+      };
+      setMapData(rewindedData);
+    }).catch(setError);
   }, []);
 
   if (error) return <main className='min-h-screen'>{`${error}`}</main>;
   if (!mapData) return <main className='min-h-screen'>Loading...</main>;
 
-  const projection = d3.geoEqualEarth();
-  const a = d3.geoPath(projection);
-  const WIDTH = 1000;
-  const HEIGHT = 800;
-  const path = projection.fitSize([WIDTH, HEIGHT], mapData);
+  const projection = d3.geoEquirectangular();
+  const path = d3.geoPath(projection);
+  projection.fitSize([WIDTH, HEIGHT], mapData);
 
   const svg = d3.select(svgRef.current)
     .attr("width", WIDTH)
     .attr("height", HEIGHT);
+
+  const g_elements = svg.append("g").attr("class", "elements");
+  g_elements.selectAll("path")
+    .data(mapData.features)
+    .enter()
+    .append('path')
+    .attr('d', path);
 
   return <main className="min-h-screen">
     <svg ref={svgRef}></svg>
